60dfcd53150dc1a14dedc58fb772cf27bad05d8a
khalilullah al ihsan <kholil3546@gmail.com>
2026-04-23 10:04:30 +0700

    selesai

---
+++ pertemuan_10/stack.py
@@ -0,0 +1,31 @@
+import stack 
+import streamlit as st
+
+st.set_page_config(page_title="Stack Simulation", layout="wide")
+
+# belum selesai, masih dalam proses pembuatan
+
+class stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, data):
+        self.stack.append(data)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.stack.pop()
+        else:
+            raise IndexError("Stack is empty")
+
+    def peek(self):
+        if not self.is_empty():
+            return self.stack[-1]
+        else:
+            raise IndexError("Stack is empty")
+
+    def is_empty(self):
+        return len(self.stack) == 0
+
+    def size(self):
+        return len(self.stack)
